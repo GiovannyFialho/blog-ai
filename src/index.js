f01a704b5e610355015d82f71ace793df563211f
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-09-08 18:48:57 -0300

    creating POST route and catching the data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import http from "node:http";
 
-const server = http.createServer((_, res) => {
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+
+  const path = url.split("?")[0];
+
+  if (path === "/sign-in" && method === "POST") {
+    let bodyBuffer = [];
+    let body = null;
+
+    req.on("data", (chunk) => bodyBuffer.push(chunk));
+    req.on("end", () => {
+      body = Buffer.concat(bodyBuffer).toString();
+      body = JSON.parse(body);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(
+        JSON.stringify({ message: "Sign-in successful", data: body }),
+      );
+    });
+
+    return;
+  }
+
   res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Hello World!!" }));
+  return res.end(JSON.stringify({ message: "Hello World!!" }));
 });
 
 server.listen(8080, () => {
