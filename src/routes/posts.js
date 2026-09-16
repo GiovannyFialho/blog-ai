6459091d621e0084b735ce723bc7fa890c6b0367
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-09-16 18:06:40 -0300

    feat(posts): create a new route to approve a post

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,6 +1,7 @@
 import { readJsonBody } from "../http/read-json-body.js";
 import {
-  findAllPosts,
+  approvePostById,
+  findAllPublishedAndApprovedPosts,
   findPostById,
   insertPost,
 } from "../repositories/post-repository.js";
@@ -8,7 +9,7 @@ import { createPostDraft } from "../services/create-post-draft.js";
 
 export function registerPostRoutes(router) {
   router.get("/posts", async (_req, res) => {
-    const posts = await findAllPosts();
+    const posts = await findAllPublishedAndApprovedPosts();
 
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ data: posts }));
@@ -16,6 +17,20 @@ export function registerPostRoutes(router) {
 
   router.get("/posts/:id", async (req, res) => {
     const post = await findPostById(req.params.id);
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ data: post }));
+  });
+
+  router.patch("/posts/:id/approve", async (req, res) => {
+    const post = await approvePostById(req.params.id);
+
+    if (!post) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Post not found" }));
+
+      return;
+    }
 
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ data: post }));
