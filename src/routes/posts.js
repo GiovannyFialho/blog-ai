32db4ac4c98f91510e5dc03a2eeabd3fb6b02240
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-09-16 18:32:07 -0300

    feat(post): create a new route to reject a post

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,6 +4,7 @@ import {
   findAllPublishedAndApprovedPosts,
   findPostById,
   insertPost,
+  rejectPostById,
 } from "../repositories/post-repository.js";
 import { createPostDraft } from "../services/create-post-draft.js";
 
@@ -24,6 +25,20 @@ export function registerPostRoutes(router) {
 
   router.patch("/posts/:id/approve", async (req, res) => {
     const post = await approvePostById(req.params.id);
+
+    if (!post) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Post not found" }));
+
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ data: post }));
+  });
+
+  router.delete("/posts/:id/reject", async (req, res) => {
+    const post = await rejectPostById(req.params.id);
 
     if (!post) {
       res.writeHead(404, { "Content-Type": "application/json" });
